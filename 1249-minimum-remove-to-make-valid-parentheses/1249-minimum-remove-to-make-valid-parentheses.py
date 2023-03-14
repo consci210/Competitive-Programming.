88acd09d9240154988aef8de26c81baa912c6bc7
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-03-14 22:51:25 +0300

    Time: 80 ms (87.99%), Space: 15.8 MB (60.54%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,26 +1,43 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        
-        is_ok = [0 for _ in range(len(s))]
+    
         stack = []
-        for (i , letter) in enumerate(s) :
-            if (letter =="("):
+        l = list(s)
+        for i in range(len(s)):
+            if l[i] == "(":
                 stack.append(i)
-            elif (letter == ")"):
+            elif l[i] == ")" :
                 if stack :
-                    ok_open = stack.pop()
-                    is_ok[ok_open] = 1 
-                    is_ok[i] = 1
-                
-        answer = ""
-        for (i,letter) in enumerate(s) :
-            if letter in "()":
-                if is_ok[i] == 1 :
-                    answer+=letter
-            else :
-                answer+= letter 
+                    stack.pop()
+                else :
+                    l[i] = ""
+        while stack :
+            value = stack.pop()
+            l[value] = ""
+            
+        return "".join(l)
         
-        return answer 
+        
+#         is_ok = [0 for _ in range(len(s))]
+#         stack = []
+#         for (i , letter) in enumerate(s) :
+#             if (letter =="("):
+#                 stack.append(i)
+#             elif (letter == ")"):
+#                 if stack :
+#                     ok_open = stack.pop()
+#                     is_ok[ok_open] = 1 
+#                     is_ok[i] = 1
+                
+#         answer = ""
+#         for (i,letter) in enumerate(s) :
+#             if letter in "()":
+#                 if is_ok[i] == 1 :
+#                     answer+=letter
+#             else :
+#                 answer+= letter 
+        
+#         return answer 
                     
             
                     
