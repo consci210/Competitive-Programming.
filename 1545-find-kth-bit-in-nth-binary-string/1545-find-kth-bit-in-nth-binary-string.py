151cf63b78750d6dbe7bd3ca7ff05bf799ee89b5
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-03-19 02:14:34 +0300

    Time: 27 ms (96.04%), Space: 13.8 MB (88.87%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,6 +1,6 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        i, s, hash_map = 1, '0', {'1': '0', '0': '1'}
-        for i in range(1, n):
-            s = s + '1' + ''.join((hash_map[i] for i in s))[::-1]
-        return s[k-1]
+        if k == 1: return "0"
+        if k == 2**(n-1): return "1"
+        if k < 2**(n-1): return self.findKthBit(n-1, k)
+        return "0" if self.findKthBit(n-1, 2**n-k) == "1" else "1"
