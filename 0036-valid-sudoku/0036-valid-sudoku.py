dc2e54d0a145df55e0e352df8b7e2392e19b8167
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-03-22 23:05:40 +0300

    Time: 103 ms (38.13%), Space: 13.8 MB (72.28%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -2,24 +2,15 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         seen_column = { 0 : [],1 : [],2 : [],3 : [],4 : [], 5: [],6 : [],7 : [],8 : []}  
         seen_box = { (0,0) : [] ,(0,1) : [] , (0,2) : [], (1,0) : [] ,(1,1) : [], (1,2) : [], (2,0) : [] ,(2,1) : [] ,(2,2): []}  
+        seen_row = { 0 : [],1 : [],2 : [],3 : [],4 : [], 5: [],6 : [],7 : [],8 : []}  
         for i in range(9):
-            seen_row = {}
             for j in range(9):
                 if board[i][j] == ".":
                     continue 
-                if board[i][j] in seen_box[ (i//3,j//3) ]:
+                if board[i][j] in seen_box[ (i//3,j//3)] or board[i][j] in seen_column[j] or board[i][j] in seen_row[i]  :
                     return False 
-                else :
-                    seen_box[(i//3,j//3) ].append(board[i][j])
-                if board[i][j] in seen_column[j] :
-                    return False 
-                else:
-                    
-                    seen_column[j].append(board[i][j])
-                if board[i][j] in seen_row :
-                    return False 
-                else:
-                    
-                    seen_row[board[i][j]] = board[i][j]
+                seen_box[(i//3,j//3) ].append(board[i][j])
+                seen_column[j].append(board[i][j])
+                seen_row[i].append(board[i][j])
         return True 
                     
