ef3de708eb6aa716a46250664ce807c0044035f3
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-03-22 13:41:10 +0300

    Time: 109 ms (48.48%), Space: 17.6 MB (68.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = {}
-        answer = []
+      
         for word in strs:
             if ("".join(sorted(word))) in anagrams :
                 anagrams["".join(sorted(word))].append(word)
             else :
                 anagrams["".join(sorted(word))] = [word]
-        for key in anagrams :
-            answer.append(anagrams[key])
-        return answer
+        
+        return anagrams.values()
             
