ba3232b589bf78e63c0a9f28a307f92ccfff3186
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-03-28 00:15:17 +0300

    Time: 1070 ms (58.60%), Space: 25 MB (81.04%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        greatest_to_right = [0]
-        difference = 0
-        for i in range(len(prices)-1 , -1 ,-1):
-            greatest_to_right.append(max(greatest_to_right[-1] , prices[i])) 
-        greatest_to_right.reverse()
-        for i in range(len(prices)):
-            difference = max (difference , greatest_to_right[i]-prices[i])
-        return difference 
+        l = 0 
+        r = 1
+        profit = 0 
+        while r < len(prices):
+            if prices[r] > prices[l] :
+                profit = max(profit , prices[r]-prices[l])
+            else:
+                l = r
+            r+=1 
+        return profit 
+    
+    
+    # greatest_to_right = [0]
+        # difference = 0
+        # for i in range(len(prices)-1 , -1 ,-1):
+        #     greatest_to_right.append(max(greatest_to_right[-1] , prices[i])) 
+        # greatest_to_right.reverse()
+        # for i in range(len(prices)):
+        #     difference = max (difference , greatest_to_right[i]-prices[i])
+        # return difference 
