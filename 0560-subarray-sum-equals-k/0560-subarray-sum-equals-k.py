6f22ecf62f49743f34cccc8206bd80fdcff24d75
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-03-29 14:06:58 +0300

    Time: 337 ms (12.84%), Space: 16.8 MB (16.83%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -3,16 +3,13 @@ class Solution:
         total = 0
         prefix_sum = [0] 
         hashmap = {}
+        
         for i in range(len(nums)):
             prefix_sum.append(prefix_sum[-1]+nums[i])
-        print(prefix_sum)
         
         for i in range(len(prefix_sum)):         
             if (prefix_sum[i] - k) in hashmap :
-                total+= hashmap[prefix_sum[i]-k]
-            if prefix_sum[i] in hashmap :
-                hashmap[prefix_sum[i]] +=1 
-            else :
-                hashmap[prefix_sum[i]] = 1
+                total+= hashmap[prefix_sum[i]-k]           
+            hashmap[prefix_sum[i]] = hashmap.get(prefix_sum[i] , 0)+1
         return total
            
