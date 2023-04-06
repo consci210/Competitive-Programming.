99b9a9b9b9545e52aa01464d512c1db95fdc8b31
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-04-06 19:06:00 +0300

    Time: 576 ms (44.76%), Space: 36.3 MB (22.92%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,29 +1,33 @@
 class MedianFinder:
 
     def __init__(self):
-        self.length = 0 
-        self.nums = []
-
+        self.small = []
+        self.large = []
+        heapq.heapify(self.small)
+        heapq.heapify(self.large)
+        
     def addNum(self, num: int) -> None:
-        self.nums.append(num)
-        self.length += 1
+        heapq.heappush(self.small , -1* num)
         
-    def findMedian(self) -> float:
-        self.nums.sort()
-        if self.length % 2 == 0 :
-            index1 = len(self.nums)//2 
-            index2 = index1-1
-            median = (self.nums[index1]+self.nums[index2]) /2 
-            return median 
+        if self.small and self.large and self.small[0]*-1 > self.large[0] :
+            val = -1* heapq.heappop(self.small)
+            heapq.heappush(self.large, val )
         
-        else :
-            index = len(self.nums) // 2
-            return self.nums[index]
+        if len(self.small) > len(self.large)+1 :
+            val = -1* heapq.heappop(self.small)
+            heapq.heappush(self.large, val )
             
-
-
-        
-
+        if len(self.large) > len(self.small)+1 :
+            val = -1* heapq.heappop(self.large)
+            heapq.heappush(self.small, val )
+            
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large) :
+            return -1 * self.small[0]
+        elif len(self.large) > len(self.small) :
+            return self.large[0]
+        else :
+            return ((self.small[0]*-1) + self.large[0] ) / 2 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
 # obj.addNum(num)
