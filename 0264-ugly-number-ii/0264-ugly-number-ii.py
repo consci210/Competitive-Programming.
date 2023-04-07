f1f5639ce0e1002605c5ff191b0be3d636934a58
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-04-07 19:46:18 +0300

    Time: 189 ms (34.40%), Space: 13.9 MB (45.15%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,18 +1,19 @@
 class Solution:
-    def nthUglyNumber(self, n: int) -> int:
-            
+    def nthUglyNumber(self, n: int) -> int:   
             index2 = index3 = index5 = 0 
             ugly_numbers = [1]
             while len(ugly_numbers) < n :
-                smallest_ugly_number = min( ugly_numbers[index2]*2 , ugly_numbers[index3]*3 , ugly_numbers[index5]*5  )   
-                ugly_numbers.append(smallest_ugly_number)
-                
-                if ugly_numbers[-1] == ugly_numbers[index2]*2 :
+                current_ugly_numbers = [ugly_numbers[index2]*2 , ugly_numbers[index3]*3 , ugly_numbers[index5]*5 ] 
+                heapq.heapify(current_ugly_numbers)
+                smallest_ugly_number = heapq.heappop(current_ugly_numbers)
+                heapq.heappush(ugly_numbers , smallest_ugly_number)
+                if smallest_ugly_number == ugly_numbers[index2]*2 :
                     index2+=1
-                if ugly_numbers[-1] == ugly_numbers[index3]*3 :
+                if  smallest_ugly_number == ugly_numbers[index3]*3 :
                     index3+=1
-                if ugly_numbers[-1] == ugly_numbers[index5]*5 :
+                if  smallest_ugly_number == ugly_numbers[index5]*5 :
                     index5+=1
             
-            return ugly_numbers[-1]
+            
+            return (ugly_numbers[n-1])
                 
