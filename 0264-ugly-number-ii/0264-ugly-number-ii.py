f690815f7c81d9c465e7f4b6cd1c1b7315837c53
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-04-07 18:40:55 +0300

    Time: 1075 ms (6.64%), Space: 14.5 MB (6.27%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -4,21 +4,18 @@ class Solution:
         count = 0 
         seen = {} 
         store = []
-#         1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 27, 30, 32, 36
         while True :
             nxt = [ugly[count]*2 , ugly[count]*3 , ugly[count]*5]
             for num in nxt :
                 if num not in seen :
-                    store.append(num)
+                    heapq.heappush(store,num)
             count+=1  
             found = False 
             while not found :
                 small = heapq.heappop(store)
                 if small not in seen :
-                    ugly.append(small)
+                    heapq.heappush(ugly , small)
                     found = True 
                 seen[small] = 1
-            if count == n*3 :
-                ugly.sort()
-                print(ugly)
+            if count == n*3 :  
                 return ugly[n-1]
