23af94e57e02fa9448904ab77d2bc394d7cb2fac
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-04-23 20:21:16 +0300

    Time: 39 ms (75.66%), Space: 14.2 MB (16.21%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         answer = []
-        
-        def permutations(arr=[], used={}):
-            if len(arr) == len(nums) :
+
+        def permutation(idx=0, arr=nums):
+            if idx == len(nums):
                 answer.append(arr.copy())
-                return 
-            for i in range(len(nums)):
-                if i not in used :
-                    used[i] = 1 
-                    arr.append(nums[i])
-                    permutations(arr , used )
-                    arr.pop()
-                    del used[i]
-                    
-        permutations()
+                return
+
+            for i in range(idx, len(nums)):
+                arr_copy = arr.copy()
+                arr_copy[i], arr_copy[idx] = arr_copy[idx], arr_copy[i]
+                permutation(idx+1, arr_copy)
+
+        permutation()
         return answer
