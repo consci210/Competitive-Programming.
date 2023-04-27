c19f8440a938bb5361f1de50a0aebf4eca1e14fd
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-04-27 20:46:53 +0300

    Time: 7914 ms (43.96%), Space: 16.4 MB (8.63%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,19 +1,24 @@
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool: 
+    def exist(self, board: List[List[str]], word: str) -> bool:
         rows = len(board)
         cols = len(board[0])
-        path = set()
-        def dfs(r , c , i ) :   
-            if i == len(word) :
+        seen = set()
+        
+        def dfs(r , c , i ) :
+            if i == len(word):
                 return True 
-            if (r<0 or c<0 or r >= rows or c>= cols or word[i]!=board[r][c] or (r,c) in path) :
+            if (r<0 or c<0 or r>=rows or c>= cols or (r,c)in seen or word[i]!= board[r][c]):
                 return False 
-            path.add((r,c))
-            res = (dfs(r+1 ,c , i+ 1) or dfs(r - 1 ,c , i+ 1) or dfs(r ,c+1, i+ 1) or  dfs(r ,c-1 , i+ 1) )
-            path.remove((r,c))
-            return res
+            
+            seen.add((r,c))
+            res = (dfs(r+1 , c , i+1) or dfs(r , c+1 , i+1 ) or dfs(r-1 , c , i+1 ) or dfs(r , c-1 , i+1))
+            seen.remove((r,c))
+            return res 
+        
+        
         for r in range(rows):
             for c in range(cols):
-                if dfs(r,c,0):
-                    return True     
-        return False 
+                if dfs(r,c,0) :
+                    return True  
+                
+        return False  
