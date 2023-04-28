7fe5aa2bfe2725e590ce6881e1f7c483b443c986
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-04-28 12:21:46 +0300

    Added solution - LeetHub

--- Permutations of a given string - GFG/permutations-of-a-given-string.py
+++ Permutations of a given string - GFG/permutations-of-a-given-string.py
@@ -2,28 +2,26 @@
 
 class Solution:
     def find_permutation(self,S):
-        result = []  # Result vector to store permutations
-        chars = sorted(list(S))  # Convert string to sorted character array
-
-    # Recursive function to find permutations
+        result = [] 
+        chars = sorted(list(S))  
         def permute(start, end):
             if start == end:
-                # Reached the end of a permutation, add to result
+               
                 result.append(''.join(chars))
             else:
-                visited = set()  # Set to track visited characters
+                visited = set()  
                 for i in range(start, end + 1):
                     if chars[i] in visited:
-                        continue  # Skip redundant characters
+                        continue 
                     visited.add(chars[i])
-                    # Swap characters
+                   
                     chars[start], chars[i] = chars[i], chars[start]
-                    # Recursively permute the remaining characters
+                   
                     permute(start + 1, end)
-                    # Restore the original order by swapping back
+                    
                     chars[start], chars[i] = chars[i], chars[start]
     
-        permute(0, len(chars) - 1)  # Start the permutation from index 0
+        permute(0, len(chars) - 1)  
         return sorted(result)
 
 
