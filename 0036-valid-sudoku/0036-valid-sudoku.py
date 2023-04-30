8abe813ebdc2ea12d0dd1bec0d3493690be3c10a
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-04-30 14:49:52 +0300

    Time: 119 ms (5.85%), Space: 16.5 MB (5.41%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,16 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        seen_column = { 0 : [],1 : [],2 : [],3 : [],4 : [], 5: [],6 : [],7 : [],8 : []}  
-        seen_box = { (0,0) : [] ,(0,1) : [] , (0,2) : [], (1,0) : [] ,(1,1) : [], (1,2) : [], (2,0) : [] ,(2,1) : [] ,(2,2): []}  
-        seen_row = { 0 : [],1 : [],2 : [],3 : [],4 : [], 5: [],6 : [],7 : [],8 : []}  
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] == ".":
-                    continue 
-                if board[i][j] in seen_box[ (i//3,j//3)] or board[i][j] in seen_column[j] or board[i][j] in seen_row[i]  :
+        boxes = { (0,0) : [] , (0,1) : [] , (0,2) : [] , (1,0) : [] , (1,1) : [] , (1,2) : [] , (2,0) : [] , (2,1) : [] , (2,2) : []  }
+        row = { 0: [] , 1:[] , 2 : [] , 3:[] , 4:[] , 5:[] , 6:[] , 7:[] , 8:[] }
+        col = { 0: [] , 1:[] , 2 : [] , 3:[] , 4:[] , 5:[] , 6:[] , 7:[] , 8:[] }
+    
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] in row[r] or board[r][c] in col[c] or board[r][c] in boxes[(r//3 , c//3)] :
                     return False 
-                seen_box[(i//3,j//3) ].append(board[i][j])
-                seen_column[j].append(board[i][j])
-                seen_row[i].append(board[i][j])
+                if board[r][c] != "." :
+                    row[r].append(board[r][c])
+                    col[c].append(board[r][c])
+                    boxes[(r//3 , c//3)].append( board[r][c])
+                
         return True 
-                    
+            
+            
+        
