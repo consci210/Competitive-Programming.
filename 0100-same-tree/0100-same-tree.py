508b33add385015055e4b640cd990ef1f4c17c04
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-05-02 22:54:03 +0300

    Time: 49 ms (5.16%), Space: 16.3 MB (6.93%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:  # Either p or q is None
+            return False
+        if p.val != q.val:
+            return False
+        # Recursive calls with return statement
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
