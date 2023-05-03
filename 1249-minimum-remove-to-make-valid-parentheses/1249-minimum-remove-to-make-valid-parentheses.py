ba240a32a7cb34cb66680ace3833eed03a7032e5
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-05-03 18:21:24 +0300

    Time: 94 ms (63.92%), Space: 18.2 MB (5.60%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,10 +1,9 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-    
         stack = []
         l = list(s)
         for i in range(len(s)):
-            if l[i] == "(":
+            if l[i] == "(" :
                 stack.append(i)
             elif l[i] == ")" :
                 if stack :
@@ -12,32 +11,7 @@ class Solution:
                 else :
                     l[i] = ""
         while stack :
-            value = stack.pop()
-            l[value] = ""
+            ch = stack.pop()
+            l[ch] = ""
             
         return "".join(l)
-        
-        
-#         is_ok = [0 for _ in range(len(s))]
-#         stack = []
-#         for (i , letter) in enumerate(s) :
-#             if (letter =="("):
-#                 stack.append(i)
-#             elif (letter == ")"):
-#                 if stack :
-#                     ok_open = stack.pop()
-#                     is_ok[ok_open] = 1 
-#                     is_ok[i] = 1
-                
-#         answer = ""
-#         for (i,letter) in enumerate(s) :
-#             if letter in "()":
-#                 if is_ok[i] == 1 :
-#                     answer+=letter
-#             else :
-#                 answer+= letter 
-        
-#         return answer 
-                    
-            
-                    
