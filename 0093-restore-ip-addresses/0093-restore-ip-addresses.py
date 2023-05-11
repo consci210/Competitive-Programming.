326b3afef1aa1d1afeab070876fb6002a2959ca2
Helina Ashenafi <106769498+consci210@users.noreply.github.com>
2023-05-11 10:10:32 +0300

    Time: 59 ms (19.49%), Space: 16.4 MB (10.14%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,19 +1,22 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
+        
         res = []
-        self.helper(s, 0, [], res)
-        return res
-    
-    def helper(self, s, start, path, res):
-        if len(path) == 4 and start == len(s):
-            res.append(".".join(path))
-            return
-        if len(path) == 4 or start == len(s):
-            return
-        for i in range(start, min(start+3, len(s))):
-            if i > start and s[start] == '0':
-                break
-            if int(s[start:i+1]) <= 255:
-                path.append(s[start:i+1])
-                self.helper(s, i+1, path, res)
-                path.pop()
+        
+        if len(s) > 12 :
+            return res
+        
+        def backtrack(start=0 , dots=0 , curr="" ):
+            if dots == 4 and start == len(s):
+                res.append(curr[:-1:])
+                return 
+            if dots >= 4 :
+                return 
+            
+            for end in range(start , min(start+3 , len(s))):
+                if ((int(s[start:end+1]) < 256 ) and ((start == end) or (s[start] != "0"))) :
+                    backtrack( end + 1 , dots+1 , curr+s[start:end+1]+ ".")
+            
+        
+        backtrack()
+        return res 
