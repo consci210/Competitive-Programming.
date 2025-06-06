113fd8b76b13daf70e98ea5a542618adb6198790
Helina <106769498+consci210@users.noreply.github.com>
2025-06-06 09:32:39 -0400

    Time: 24 ms (33.9%), Space: 18 MB (34.83%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,22 +1,12 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         letters = defaultdict(int)
-        answers = defaultdict(int)
+        for l in magazine:
+            letters[l]+=1
+        
         for l in ransomNote:
-            if l in letters:
-                letters[l]+=1
-            else:
-                letters[l] = 1
-        
-        for a in magazine:
-            if a in answers:
-                answers[a]+=1
-            else:
-                answers[a]= 1
-        
-        for l in letters:
-            if letters[l] > answers[l]:
+            letters[l]-=1 
+            if letters[l] < 0:
                 return False 
         return True 
-
             
