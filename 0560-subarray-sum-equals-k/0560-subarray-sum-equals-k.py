0e53ef258b79340e20862d90e071752b2f51aae7
Helina <106769498+consci210@users.noreply.github.com>
2025-06-11 20:00:09 -0400

    Time: 47 ms (11.43%), Space: 22.5 MB (5.46%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,15 +1,19 @@
+from collections import defaultdict
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        prefixSum = [0 for _ in range(len(nums))]
         total = 0
-        prefix_sum = [0] 
-        hashmap = {}
-        
+        count = 0
+        complement = defaultdict(int)
+        complement[0] = 1  # base case to count subarrays starting at index 0
+
         for i in range(len(nums)):
-            prefix_sum.append(prefix_sum[-1]+nums[i])
-        
-        for i in range(len(prefix_sum)):         
-            if (prefix_sum[i] - k) in hashmap :
-                total+= hashmap[prefix_sum[i]-k]           
-            hashmap[prefix_sum[i]] = hashmap.get(prefix_sum[i] , 0)+1
-        return total
-           
+            total += nums[i]
+            prefixSum[i] = total
+
+        for subArray in prefixSum:
+            count += complement[subArray - k]
+            complement[subArray] += 1
+
+        return count
