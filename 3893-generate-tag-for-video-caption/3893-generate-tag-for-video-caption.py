35df843160e833e6a8227e6d6b4d0455e663fab1
Helina <106769498+consci210@users.noreply.github.com>
2025-06-15 13:53:16 -0400

    Time: 0 ms (100%), Space: 17.7 MB (100%) - LeetHub

--- 3893-generate-tag-for-video-caption/3893-generate-tag-for-video-caption.py
+++ 3893-generate-tag-for-video-caption/3893-generate-tag-for-video-caption.py
@@ -1,11 +1,9 @@
 class Solution:
     def generateTag(self, caption: str) -> str:
         words = caption.split()
-        print(words)
         if not words:
             return "#"
         tag = "#" + words[0].lower()
-        for i in range(1,len(words)):
-            tag+=words[i][0].upper()
-            tag+=words[i][1:].lower()
+        for word in words[1:]:
+            tag += word[0].upper() + word[1:].lower()
         return tag[:100]
