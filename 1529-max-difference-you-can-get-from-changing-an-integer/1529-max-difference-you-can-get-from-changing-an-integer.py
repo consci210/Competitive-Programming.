0fc80ec90a77dd3520e6eb01c98610b698a2282a
Helina <106769498+consci210@users.noreply.github.com>
2025-06-15 13:10:08 -0400

    Time: 0 ms (100%), Space: 17.6 MB (88.27%) - LeetHub

--- 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.py
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.py
@@ -1,13 +1,26 @@
 class Solution:
     def maxDiff(self, num: int) -> int:
-        number = str(num)
-        x = set(number)
-        small = str(num)
-        large = str (num)
-        for i in range(0,10):
-            for j in x:
-                if number[0] != j or i != 0:
-                    curr = number.replace(j,str(i))
-                    small = str(min(int(curr),int(small)))
-                    large = str(max(int(curr),int(large)))
-        return (int(large)- int(small))
+        num_str = str(num)
+
+        # Get max number by replacing the first non-9 digit with 9
+        for ch in num_str:
+            if ch != '9':
+                max_str = num_str.replace(ch, '9')
+                break
+        else:
+            max_str = num_str  # All digits are already 9
+
+        # Get min number by replacing:
+        # If first digit is not '1', replace it with '1' to avoid leading zero
+        if num_str[0] != '1':
+            min_str = num_str.replace(num_str[0], '1')
+        else:
+            # First digit is already '1', so replace next non-0, non-1 digit with '0'
+            for ch in num_str[1:]:
+                if ch != '0' and ch != '1':
+                    min_str = num_str.replace(ch, '0')
+                    break
+            else:
+                min_str = num_str  # No change needed if already minimal
+
+        return int(max_str) - int(min_str)
